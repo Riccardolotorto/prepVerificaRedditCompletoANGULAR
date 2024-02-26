d226ce7b132d47b8e92453fdf3a4e19820126aa2
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2024-02-26 11:49:57 +0000

    punto 5

--- email-lotorto/src/app/app.module.ts
+++ email-lotorto/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SentMailComponent } from './sent-mail/sent-mail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SentMailComponent
   ],
   imports: [
     BrowserModule,
