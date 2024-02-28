1b7d352b9ca5cf6794568fbd0ce3f1d534f878dc
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2024-02-28 11:16:54 +0000

    punto 7-8 seconda esercitazione

--- email-lotorto2/src/app/sent-mail/sent-mail.component.ts
+++ email-lotorto2/src/app/sent-mail/sent-mail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
 import { Email } from '../email.model';
 
 @Component({
@@ -9,8 +9,17 @@ import { Email } from '../email.model';
 export class SentMailComponent {
   @Input() mails: Email[] = [];
   nascondi: boolean = false;
+  @HostBinding('attr.class') cssClass= "card bg-light mt-3";
   espandi(): boolean {
     this.nascondi = !this.nascondi;
+    return false
+  }
+  speciale(): boolean {
+    if(this.cssClass = "card bg-warning mt-3") {
+      this.cssClass = "card bg-light mt-3";
+    } if (this.cssClass = "card bg-light mt-3") {
+      this.cssClass = "card bg-warning mt-3";
+    }
     return false
   }
 }
