9f309ed7c3c7af290caf681a4623b5873fb6c169
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2024-02-28 11:06:51 +0000

    punto 5 seconda esercitazione

---
+++ email-lotorto2/src/app/sent-mail/sent-mail.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { Email } from '../email.model';
+
+@Component({
+  selector: 'app-sent-mail',
+  templateUrl: './sent-mail.component.html',
+  styleUrls: ['./sent-mail.component.css']
+})
+export class SentMailComponent {
+  @Input() mails: Email[] = [];
+}
