ca98a7b3ff68489350e3cf981e8af073a6aff1f3
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2024-02-28 10:58:15 +0000

    punto 4 seconda esercitazione

--- email-lotorto2/src/app/app.component.ts
+++ email-lotorto2/src/app/app.component.ts
@@ -7,5 +7,9 @@ import { Email } from './email.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  emails: Email[] = [];
+  invia(d: HTMLInputElement, o: HTMLInputElement, c: HTMLInputElement): boolean {
+    this.emails.push(new Email(d.value, o.value, c.value));
+    return false
+  }
 }
