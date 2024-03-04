8c2460bb29b0a1ca9a3ad78e11b0fe856070f82b
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2024-03-04 11:27:21 +0000

    punto 7-8 terza es

--- email-lotorto3/src/app/sent-mail/sent-mail.component.ts
+++ email-lotorto3/src/app/sent-mail/sent-mail.component.ts
@@ -9,8 +9,17 @@ import { Email } from '../email.model';
 export class SentMailComponent {
   @Input() mails: Email[] = [];
   nascondi: boolean = false;
+  @HostBinding('attr.class') cssClass = 'card bg-light mt-3'
   espandi(): boolean {
     this.nascondi = !this.nascondi;
+    return false
+  }
+  speciale(): boolean {
+    if (this.cssClass == 'card bg-warning mt-3') {
+      this.cssClass = 'card bg-light mt-3';
+    } else {
+      this.cssClass = 'card bg-warning mt-3';
+    }
     return false
   }
 }
