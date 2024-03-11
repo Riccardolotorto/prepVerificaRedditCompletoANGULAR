4f221f9305dd0ceebafac4f23fa2f94c65eed029
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2024-03-11 18:57:42 +0000

    punto 3

---
+++ email-sandro/src/app/email.model.ts
@@ -0,0 +1,12 @@
+export class Email {
+    destinatario: string;
+    oggetto: string;
+    corpo: string;
+
+    constructor(destinatario:string, oggetto:string, corpo:string) {
+        this.destinatario = destinatario;
+        this.oggetto = oggetto;
+        this.corpo = corpo;
+    }
+    
+}
