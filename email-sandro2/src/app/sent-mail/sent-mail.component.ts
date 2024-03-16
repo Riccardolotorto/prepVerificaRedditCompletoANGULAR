27179e42e29088e683829f3f002b92674dc1c36a
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2024-03-16 11:43:31 +0000

    punto 8

--- email-sandro2/src/app/sent-mail/sent-mail.component.ts
+++ email-sandro2/src/app/sent-mail/sent-mail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input} from '@angular/core';
+import { Component, HostBinding, Input} from '@angular/core';
 import { Email } from '../email.model';
 
 @Component({
@@ -9,8 +9,17 @@ import { Email } from '../email.model';
 export class SentMailComponent {
   @Input() mailozza !: Email;
   nascondi: boolean = false;
+  @HostBinding('attr.class') cssClass = "card bg-light"
   espandi(): boolean {
     this.nascondi = !this.nascondi;
+    return false
+  }
+  speciale():boolean {
+    if (this.cssClass == "card bg-warning") {
+      this.cssClass = "card bg-light";
+    } else {
+      this.cssClass = "card bg-warning";
+    }
     return false
   }
 }
